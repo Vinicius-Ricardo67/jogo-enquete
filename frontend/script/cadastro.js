88ee0456ed3065701f10873d06da867dcdf39c08
Vinicius-ricardo <vinicius.ricardo@xpert.com.br>
2025-12-03 16:29:04 -0300

    refactor(server): improve server run logic

--- frontend/script/cadastro.js
+++ frontend/script/cadastro.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/cadastro", {
+      const response = await fetch("http://192.168.1.90:8000/cadastro", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nome, email, senha }),
