ecfadb7f38b15c0766fc264327441efc1110033e
Vinicius-ricardo <vinicius.ricardo@xpert.com.br>
2026-01-06 11:22:10 -0300

    finishing the game 3

--- frontend/script/isso_ou_aquilo.js
+++ frontend/script/isso_ou_aquilo.js
@@ -6,23 +6,24 @@ const estado = {
   vencedor: null,
   tempo: 10,
   intervalo: null,
-  duelo: null
+  duelo: null,
+  totalDuelos: 0,
+  duelosJogados: 0
 };
 
 const ui = {
   telaCategorias: document.getElementById("tela-categorias"),
   telaJogo: document.getElementById("tela-jogo"),
   telaFim: document.getElementById("fim"),
-
   pergunta: document.getElementById("perguntas"),
   label1: document.getElementById("label1"),
   label2: document.getElementById("label2"),
   img1: document.getElementById("img1"),
   img2: document.getElementById("img2"),
-  timer: document.getElementById("timer"),
-
   opcao1: document.getElementById("opcao1"),
-  opcao2: document.getElementById("opcao2")
+  opcao2: document.getElementById("opcao2"),
+  timer: document.getElementById("timer"),
+  barraProgresso: document.getElementById("progresso-barra")
 };
 
 
@@ -39,13 +40,14 @@ function escolherCategoria(cat) {
   estado.categoria = cat;
 
   const subcategorias = Object.keys(estado.dados.categorias[cat]);
-  estado.subcategoria =
-    subcategorias[Math.floor(Math.random() * subcategorias.length)];
+  estado.subcategoria = subcategorias[Math.floor(Math.random() * subcategorias.length)];
 
   const lista = estado.dados.categorias[cat][estado.subcategoria];
-  estado.pool = [...lista];
+  estado.pool = selecionarPergunta(lista, 15);
   estado.vencedor = null;
 
+  estado.totalDuelos = estado.pool.length - 1;
+  estado.duelosJogados = 0;
 
   console.log("Categoria:", cat);
   console.log("Subcategoria:", estado.subcategoria);
@@ -58,7 +60,16 @@ function escolherCategoria(cat) {
 }
 
 
+function atualizarProgresso() {
+  const pct = (estado.duelosJogados / estado.totalDuelos) * 100;
+
+  ui.barraProgresso.style.width = `${pct}%`;
+}
+
+
 function proximaRodada() {
+  estado.duelosJogados++;
+  atualizarProgresso();
   clearInterval(estado.intervalo);
 
   if (estado.pool.length === 0 && estado.vencedor) {
@@ -104,7 +115,7 @@ function escolher(opcao) {
 
 
 function iniciarTimer() {
-  estado.tempo = 10;
+  estado.tempo = 1000;
   ui.timer.textContent = estado.tempo;
 
   estado.intervalo = setInterval(() => {
@@ -131,4 +142,17 @@ function finalizarJogo() {
 function reiniciarJogo() {
   ui.telaFim.classList.add("hidden");
   ui.telaCategorias.classList.remove("hidden");
+}
+
+function embaralhar(array) {
+  const copia = [...array];
+  for (let i = copia.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [copia[i], copia[j]] = [copia[j], copia[i]];
+  }
+  return copia
+}
+
+function selecionarPergunta(opcoes, quantidade = 15) {
+  return embaralhar(opcoes).slice(0, quantidade);
 }
